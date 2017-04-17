97df6c199f70615e6676e2c837ca889a8d0ec8ad
Tom Dooner <tom.dooner@brigade.com>
2017-04-17 14:24:23 -0700

    Add #offender_details method extracted from myadvocate
    This is a generally useful method to give information about the offender
    being looked at.

--- lib/oos_mechanizer.rb
+++ lib/oos_mechanizer.rb
@@ -46,6 +46,42 @@ module OosMechanizer
       raise OosMechanizer::Searcher::ResponseCodeError
     end
 
+    def offender_details(sid)
+      results_page = @search_page.form_with(id: 'mainBodyForm') do |f|
+        f['mainBodyForm:SidNumber'] = sid
+      end.click_button
+
+      offenses = results_page.css('[id="offensesForm:offensesTable"] tbody tr:nth-child(2n+1)')
+
+      return nil if results_page.css('#errorMessages').text =~ /invalid characters detected/i
+      return nil if results_page.css('#errorMessages').text =~ /no matching records/i
+
+      return {
+        sid: results_page.css('[id="offensesForm:out_SID"]').text,
+        name: results_page.css('[id="offensesForm:name"]').text,
+
+        age: results_page.css('[id="offensesForm:age"]').text,
+        gender: results_page.css('[id="offensesForm:sex"]').text,
+        height: results_page.css('[id="offensesForm:height"]').text,
+        weight: results_page.css('[id="offensesForm:weight"]').text,
+        dob: results_page.css('[id="offensesForm:dob"]').text,
+        race: results_page.css('[id="offensesForm:race"]').text,
+        hair: results_page.css('[id="offensesForm:hair"]').text,
+        eyes: results_page.css('[id="offensesForm:eyes"]').text,
+
+        caseload_number: results_page.css('[id="offensesForm:caseloadNumber"]').text,
+        caseload_name: results_page.css('[id="offensesForm:caseloadMgrsTable"]').text.strip,
+
+        location: results_page.css('[id="offensesForm:locationBlock"] td:nth-child(2)').text,
+        status: results_page.css('[id="offensesForm:status"]').text,
+        admission_date: results_page.css('[id="offensesForm:admitDate"]').text,
+        earliest_release_date: results_page.css('[id="offensesForm:relDate"]').text,
+
+        offenses: offenses.map {|o| o.text.strip.gsub(/\s+/, ',') },
+        num_offenses: offenses.length,
+      }
+    end
+
     private
 
     def process_results(results_page)
